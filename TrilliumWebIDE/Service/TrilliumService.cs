21add76ccebdc671364d89d495febe88c0867032
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-30 19:34:59 -0500

    tIDE initial commit with assets

---
+++ TrilliumWebIDE/Service/TrilliumService.cs
@@ -0,0 +1,14 @@
+﻿
+using TrilliumWebIDE.Trillium;
+
+namespace TrilliumWebIDE.Service
+{
+    public class TrilliumService
+    {
+        public TrilliumRepl? TrillRepl()
+        {
+            var repl = new TrilliumRepl();
+            return repl;
+        }
+    }
+}
